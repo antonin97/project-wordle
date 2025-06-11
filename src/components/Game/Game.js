86d67c7cdf4d9d52d90adad68971e6243451ff32
Antonin Martykan <a.martykan@gmail.com>
2025-06-11 09:32:34 +0200

    stretch 2 done

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -10,6 +10,9 @@ import LoseBanner from "../LoseBanner";
 import Keyboard from "../Keyboard";
 
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { KEYS } from "../../constants";
+
+import { checkGuess } from "../../game-helpers";
 
 // Pick a random word on every pageload.
 //
@@ -20,21 +23,74 @@ console.info({ answer });
 function Game() {
     const [guesses, setGuesses] = React.useState([]);
     const [gameOutcome, setGameOutcome] = React.useState(0); // 0 = ongoing, 1 = win, -1 = lose
+    const [keyDict, setKeyDict] = React.useState(() => {
+        let dict = {};
+
+        for (let row of KEYS) {
+            for (let letter of row) {
+                dict[letter] = null;
+            }
+
+            return dict;
+        }
+    });
+
+    function updateDict(guesses) {
+        const checkedArrays = guesses.map((object) => {
+            return checkGuess(object.guess, answer);
+        });
+
+        if (checkedArrays.length === 0) {
+            return;
+        }
+
+        const updatedDict = { ...keyDict };
+
+        let lastArr = checkedArrays.slice(-1)[0];
+        for (let { letter, status } of lastArr) {
+          if (status === "incorrect") {
+              updatedDict[letter] = "incorrect";
+          }
+          
+          if (
+              status === "misplaced" &&
+              updatedDict[letter] !== "correct"
+          ) {
+              updatedDict[letter] = "misplaced";
+          }
+          
+          if (status === "correct") {
+              updatedDict[letter] = "correct";
+          }
+        }
+
+        setKeyDict(updatedDict);
+    }
 
     function restart() {
         setGuesses([]);
         setGameOutcome(0);
+        setKeyDict(() => {
+            let dict = {};
+
+            for (let row of KEYS) {
+                for (let letter of row) {
+                    dict[letter] = null;
+                }
+
+                return dict;
+            }
+        });
         answer = sample(WORDS);
         console.info({ answer });
     }
 
-
     function handleGuesses(guess) {
-        
-
         const newGuesses = [...guesses, { guess, id: crypto.randomUUID() }];
 
         setGuesses(newGuesses);
+
+        updateDict(newGuesses);
 
         if (guess === answer) {
             setGameOutcome(1); // Win
@@ -44,13 +100,14 @@ function Game() {
         if (NUM_OF_GUESSES_ALLOWED <= newGuesses.length) {
             setGameOutcome(-1); // Lose
         }
+
         
     }
 
     return (
         <>
             <UserGuesses guesses={guesses} answer={answer} />
-            <Keyboard guesses={guesses} answer={answer} />
+            <Keyboard keyDict={keyDict} />
             <TextInput handleGuess={handleGuesses} gameOutcome={gameOutcome} />
             {gameOutcome === 1 ? (
                 <WinBanner numOfGuesses={guesses.length} restart={restart} />
--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { KEYS } from '../../constants';
 
-function Keyboard() {
-  return <div></div>;
+
+function Keyboard({ keyDict }) {
+    return (
+      <div className="keyboard">
+          {KEYS.map((row, rowIndex) => (
+              <div key={rowIndex} className="keyboard-row">
+                  {row.map((key) => (
+                      <button
+                          key={key}
+                          className={`keyboard-key ${keyDict[key] || ''}`}
+                      >
+                          {key}
+                      </button>
+                  ))}
+              </div>
+          ))}
+      </div>
+  );
 }
 
 export default Keyboard;
